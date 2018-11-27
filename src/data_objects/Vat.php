147015e5920d9e185b4f9bfd7438b2774476bb62
POPsy <popsy2007@mail.ru>
2018-11-27 15:15:33 +0300

    Поддержка НДС 20%

--- src/data_objects/Vat.php
+++ src/data_objects/Vat.php
@@ -6,41 +6,41 @@ use Platron\AtolV4\handbooks\Vates;
 
 class Vat extends BaseDataObject
 {
-	/** @var string */
-	protected $type;
-	/** @var float */
-	protected $sum;
+    /** @var string */
+    protected $type;
+    /** @var float */
+    protected $sum;
 
-	public function __construct(Vates $type)
-	{
-		$this->type = $type->getValue();
-	}
+    public function __construct(Vates $type)
+    {
+        $this->type = $type->getValue();
+    }
 
-	public function addSum($sum)
-	{
-		$this->sum = $this->getTaxAmount($sum);
-	}
+    public function addSum($sum)
+    {
+        $this->sum = $this->getTaxAmount($sum);
+    }
 
-	/**
-	 * Получить сумму налога
-	 * @param float $amount
-	 * @return float
-	 */
-	protected function getTaxAmount($amount)
-	{
-		switch ($this->type) {
-			case Vates::NONE:
-			case Vates::VAT0:
-				return round(0, 2);
-			case Vates::VAT10:
-			case Vates::VAT110:
-				return round($amount * 10 / 110, 2);
-			case Vates::VAT18:
-			case Vates::VAT118:
-				return round($amount * 18 / 118, 2);
+    /**
+     * Получить сумму налога
+     * @param float $amount
+     * @return float
+     */
+    protected function getTaxAmount($amount)
+    {
+        switch ($this->type) {
+            case Vates::NONE:
+            case Vates::VAT0:
+                return round(0, 2);
+            case Vates::VAT10:
+            case Vates::VAT110:
+                return round($amount * 10 / 110, 2);
+            case Vates::VAT18:
+            case Vates::VAT118:
+                return round($amount * 18 / 118, 2);
             case Vates::VAT20:
             case Vates::VAT120:
                 return round($amount * 20 / 120, 2);
-		}
-	}
+        }
+    }
 }
--- src/handbooks/Vates.php
+++ src/handbooks/Vates.php
@@ -6,13 +6,13 @@ use MyCLabs\Enum\Enum;
 
 class Vates extends Enum
 {
-	const
-		NONE = 'none',
-		VAT0 = 'vat0',
-		VAT10 = 'vat10',
-		VAT18 = 'vat18',
+    const
+        NONE = 'none',
+        VAT0 = 'vat0',
+        VAT10 = 'vat10',
+        VAT18 = 'vat18',
         VAT20 = 'vat20',
-		VAT110 = 'vat110',
-		VAT118 = 'vat118',
+        VAT110 = 'vat110',
+        VAT118 = 'vat118',
         VAT120 = 'vat120';
 }
