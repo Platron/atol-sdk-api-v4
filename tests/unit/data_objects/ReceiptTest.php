5799400823cd5e8edb250aa9f9df9a05c1f018ea
v.egorov <v.egorov@platron.ru>
2019-07-09 15:34:31 +0300

    Add unit tests for Receipt class

---
+++ tests/unit/data_objects/ReceiptTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Platron\AtolV4\tests\unit\data_objects;
+
+use Platron\AtolV4\data_objects\Client;
+use Platron\AtolV4\data_objects\Company;
+use Platron\AtolV4\data_objects\Item;
+use Platron\AtolV4\data_objects\Payment;
+use Platron\AtolV4\data_objects\Receipt;
+use Platron\AtolV4\handbooks\ReceiptOperationTypes;
+
+class ReceiptTest extends \PHPUnit_Framework_TestCase
+{
+    public function test_getParameters_withoutAdditionalCheckProps_resultHasNoKey()
+    {
+        $receipt = $this->createReceipt();
+
+        $this->assertArrayNotHasKey('additional_check_props', $receipt->getParameters());
+    }
+
+    public function test_getParameters_withAdditionalCheckProps_resultHasAppropriateValue()
+    {
+        $receipt = $this->createReceipt();
+
+        $receipt->setAdditionalCheckProps('additional');
+
+        $this->assertEquals($receipt->getParameters()['additional_check_props'], 'additional');
+    }
+
+    public function test_setAdditionalCheckProps_notString_expectInvalidArgumentException()
+    {
+        $receipt = $this->createReceipt();
+
+        $this->expectException(\InvalidArgumentException::class);
+
+        $receipt->setAdditionalCheckProps(123);
+    }
+
+    public function test_setAdditionalCheckProps_longString_expectLengthException()
+    {
+        $receipt = $this->createReceipt();
+
+        $this->expectException(\LengthException::class);
+
+        $receipt->setAdditionalCheckProps('12345678901234567');
+    }
+
+    /**
+     * @return Receipt
+     */
+    private function createReceipt()
+    {
+        return new Receipt($this->createMock(Client::class), $this->createMock(Company::class),
+            [$this->createMock(Item::class)], $this->createMock(Payment::class),
+            $this->createMock(ReceiptOperationTypes::class));
+    }
+}
