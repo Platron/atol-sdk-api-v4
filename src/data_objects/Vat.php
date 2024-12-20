6418246f243557f964d18842193628536bf3e4f9
i.badamshin <i.badamshin@platron.ru>
2024-12-20 11:28:07 +0300

    add new Vates

--- src/data_objects/Vat.php
+++ src/data_objects/Vat.php
@@ -32,6 +32,12 @@ class Vat extends BaseDataObject
 			case Vates::NONE:
 			case Vates::VAT0:
 				return round(0, 2);
+			case Vates::VAT5:
+			case Vates::VAT105:
+				return round($amount * 5 / 105, 2);
+			case Vates::VAT7:
+			case Vates::VAT107:
+				return round($amount * 7 / 107, 2);
 			case Vates::VAT10:
 			case Vates::VAT110:
 				return round($amount * 10 / 110, 2);
--- src/handbooks/Vates.php
+++ src/handbooks/Vates.php
@@ -9,8 +9,12 @@ class Vates extends Enum
 	const
 		NONE = 'none',
 		VAT0 = 'vat0',
+		VAT5 = 'vat5',
+		VAT7 = 'vat7',
 		VAT10 = 'vat10',
 		VAT20 = 'vat20',
+		VAT105 = 'vat105',
+		VAT107 = 'vat107',
 		VAT110 = 'vat110',
 		VAT120 = 'vat120';
 }
